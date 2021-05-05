e9919084d966905bf9195312d24efefe3af4e363
kalagoniomprakash <62206157+kalagoniomprakash@users.noreply.github.com>
2021-05-05 21:52:01 +0200

    Update tc1.java

--- src/test/java/TestNG/tc1.java
+++ src/test/java/TestNG/tc1.java
@@ -17,7 +17,7 @@ public class tc1 {
 		  driver = new ChromeDriver();
 		  driver.manage().window().maximize();
 		  driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
-		  driver.get("http://15.207.111.226:32768/addressbook/");
+		  driver.get("http://ec2-3-128-155-151.us-east-2.compute.amazonaws.com:1234/addressbook/");
 		
 		}
 		
